c3a2e089a98ea21ef6e1690b5c65bf5e13ba00f2
Dmitry Sergeevich Tarasenko <dmitry.tarasenko@lykke.com>
2018-09-20 04:19:57 +0300

    Fix settings LPDEV-885

--- src/Lykke.Job.PayTelegramReporter.Domain/Settings/Socks5ProxySettings.cs
+++ src/Lykke.Job.PayTelegramReporter.Domain/Settings/Socks5ProxySettings.cs
@@ -1,10 +1,19 @@
-﻿namespace Lykke.Job.PayTelegramReporter.Domain.Settings
+﻿using Lykke.SettingsReader.Attributes;
+
+namespace Lykke.Job.PayTelegramReporter.Domain.Settings
 {
     public class Socks5ProxySettings
     {
+        [Optional]
         public string Host { get; set; }
+
+        [Optional]
         public int Port { get; set; }
+
+        [Optional]
         public string Username { get; set; }
+
+        [Optional]
         public string Password { get; set; }
     }
 }
